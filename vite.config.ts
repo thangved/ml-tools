1b69d23d44da3b78850fd148dd2f6457e7a69d43
Kim Minh Thang <thangved02@gmail.com>
2023-06-29 14:53:40 +0700

    feat: split chunk vendor plugin (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), splitVendorChunkPlugin()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
